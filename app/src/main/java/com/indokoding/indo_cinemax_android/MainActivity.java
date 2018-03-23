1aefc0cf02c415795dc1b8d18ea72a040838fe1d
Prayugo Dwi Setiyoko <dwi.deni24@gmail.com>
2018-03-23 22:24:04 +0700

    penambahan splash scren dan navigation drawer

--- app/src/main/java/com/indokoding/indo_cinemax_android/MainActivity.java
+++ app/src/main/java/com/indokoding/indo_cinemax_android/MainActivity.java
@@ -1,13 +1,41 @@
 package com.indokoding.indo_cinemax_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.imSplash)
+    ImageView imSplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+//        splah screen
+        Animation myAnimSplash = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        imSplash.startAnimation(myAnimSplash);
+        final Intent intent = new Intent(this, HomeActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
+
